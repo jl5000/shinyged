7b1df6d43417e80dcee7e4595967ca868ec433c1
Jamie Lendrum <jalendrum@gmail.com>
2021-05-22 21:57:03 +0100

    feat: Sort out address module and universal helper functions to update tidyged object

--- R/mod_address.R
+++ R/mod_address.R
@@ -37,55 +37,100 @@ address_ui <- function(id) {
 }
 
 
-address_server <- function(id, r) {
+address_server <- function(id, r, section_rows) {
   moduleServer(id, function(input, output, session) {
     
+    tag_order <- c("ADDR",paste0("ADR",1:3),"CITY","STAE","POST",
+                   "CTRY","EMAIL","PHON","FAX","WWW")
+    
+    section <- shiny::reactiveValues(rows = section_rows, auto_update = TRUE)
+    
     addr <- shiny::reactive({
-      req(r$ged, r$section_rows)
-      dplyr::slice(r$ged, r$section_rows) %>%
-        dplyr::filter(tag %in% c("ADDR",paste0("ADR",1:3),"CITY","STAE","POST",
-                                 "CTRY","EMAIL","PHON","FAX","WWW"))
+      req(r$ged, section$rows)
+      r$ged[section$rows,]
+    })
+    
+    base_level <- shiny::reactive({
+      req(addr)
+      addr()$level[1]
     })
     
     observeEvent(addr(), {
-      shiny::updateTextAreaInput(session = session, "adr", 
-                                 value = dplyr::filter(addr(), tag %in% paste0("ADR",1:3))$value %>% 
-                                   sort() %>% 
-                                   paste(collapse = "\n"))
-      shiny::updateTextInput(session = session, "city", 
-                             value = dplyr::filter(addr(), tag == "CITY")$value)
-      shiny::updateTextInput(session = session, "state", 
-                             value = dplyr::filter(addr(), tag == "STAE")$value)
-      shiny::updateTextInput(session = session, "postcode", 
-                             value = dplyr::filter(addr(), tag == "POST")$value)
-      shiny::updateTextInput(session = session, "country", 
-                             value = dplyr::filter(addr(), tag == "CTRY")$value)
-      shiny::updateTextAreaInput(session = session, "emails", 
-                                 value = dplyr::filter(addr(), tag == "EMAIL")$value %>% 
-                                   paste(collapse = "\n"))
-      shiny::updateTextAreaInput(session = session, "phones", 
-                                 value = dplyr::filter(addr(), tag == "PHON")$value %>% 
-                                   paste(collapse = "\n"))
-      shiny::updateTextAreaInput(session = session, "faxes", 
-                                 value = dplyr::filter(addr(), tag == "FAX")$value %>% 
-                                   paste(collapse = "\n"))
-      shiny::updateTextAreaInput(session = session, "websites", 
-                                 value = dplyr::filter(addr(), tag == "WWW")$value %>% 
-                                   paste(collapse = "\n"))
+      print(addr())
+      # Only update on initilisation
+      if(section$auto_update) {
+        shiny::updateTextAreaInput(session = session, "adr",
+                                   value = dplyr::filter(addr(), tag %in% paste0("ADR",1:3))$value %>%
+                                     paste(collapse = "\n"))
+        shiny::updateTextInput(session = session, "city",
+                               value = dplyr::filter(addr(), tag == "CITY")$value)
+        shiny::updateTextInput(session = session, "state",
+                               value = dplyr::filter(addr(), tag == "STAE")$value)
+        shiny::updateTextInput(session = session, "postcode",
+                               value = dplyr::filter(addr(), tag == "POST")$value)
+        shiny::updateTextInput(session = session, "country",
+                               value = dplyr::filter(addr(), tag == "CTRY")$value)
+        shiny::updateTextAreaInput(session = session, "emails",
+                                   value = dplyr::filter(addr(), tag == "EMAIL")$value %>%
+                                     paste(collapse = "\n"))
+        shiny::updateTextAreaInput(session = session, "phones",
+                                   value = dplyr::filter(addr(), tag == "PHON")$value %>%
+                                     paste(collapse = "\n"))
+        shiny::updateTextAreaInput(session = session, "faxes",
+                                   value = dplyr::filter(addr(), tag == "FAX")$value %>%
+                                     paste(collapse = "\n"))
+        shiny::updateTextAreaInput(session = session, "websites",
+                                   value = dplyr::filter(addr(), tag == "WWW")$value %>%
+                                     paste(collapse = "\n"))
+      }
+      section$auto_update = FALSE
     })
 
+    
+    shiny::observeEvent(input$adr, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      adr <- split_multiline(input$adr, 3)
+      dummy <- update_ged_value(r, section, tag_order, base_level() + 1, paste0("ADR", 1:3), adr)
+    })
+    shiny::observeEvent(input$city, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      dummy <- update_ged_value(r, section, tag_order, base_level() + 1, "CITY", input$city)
+    })
+    shiny::observeEvent(input$state, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      dummy <- update_ged_value(r, section, tag_order, base_level() + 1, "STAE", input$state)
+    })
+    shiny::observeEvent(input$postcode, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      dummy <- update_ged_value(r, section, tag_order, base_level() + 1, "POST", input$postcode)
+    })
+    shiny::observeEvent(input$country, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      dummy <- update_ged_value(r, section, tag_order, base_level() + 1, "CTRY", input$country)
+    })
+    shiny::observeEvent(input$emails, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      emails <- split_multiline(input$emails, 3)
+      dummy <- update_ged_value(r, section, tag_order, base_level(), "EMAIL", emails)
+    })
+    shiny::observeEvent(input$phones, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      phones <- split_multiline(input$phones, 3)
+      dummy <- update_ged_value(r, section, tag_order, base_level(), "PHON", phones)
+    })
+    shiny::observeEvent(input$faxes, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      faxes <- split_multiline(input$faxes, 3)
+      dummy <- update_ged_value(r, section, tag_order, base_level(), "FAX", faxes)
+    })
+    shiny::observeEvent(input$websites, ignoreNULL = FALSE, ignoreInit = TRUE, {
+      websites <- split_multiline(input$websites, 3)
+      dummy <- update_ged_value(r, section, tag_order, base_level(), "WWW", websites)
+    })
     
   })
 }
 
 
-address_app <- function(addr = NULL) {
-  r <- shiny::reactiveValues(ged = ged, section_rows = section_rows)
+address_app <- function(ged = NULL, section_rows = NULL) {
+  r <- shiny::reactiveValues(ged = ged)
   ui <- shiny::fluidPage(
     address_ui("address")
   )
   server <- function(input, output, session) {
-    address_server("address", r)
+    address_server("address", r, section_rows)
   }
   shiny::shinyApp(ui, server)  
 }
---
+++ R/utils.R
@@ -0,0 +1,51 @@
+
+
+#' Update value(s) in a tidyged object
+#'
+#' @param r A reactiveValues object containing the tidyged object (r$ged).
+#' @param section A reactiveValues object containing the rows of the section to update (section$rows).
+#' @param tag_order A character vector of tags indicating the desired order of tags in the section.
+#' @param lvl The level of the value(s) to update.
+#' @param tg All tag(s) that can be associated with the value(s).
+#' @param new_value A character vector of new values with the appropriate tag and level. 
+#'
+#' @return A dummy value of TRUE. The critical return values are the updated reactiveValues objects,
+#' which are updated by reference.
+update_ged_value <- function(r, section, tag_order, lvl, tags, new_value = character()) {
+  
+  for(tg in tags) if(!tg %in% tag_order) stop("Tag is not recognised")
+  if(length(new_value) == 1 && new_value == "") new_value = character()
+  
+  addr <- r$ged[section$rows,]
+  ged <- r$ged[-section$rows,]
+  
+  # Delete existing
+  new_addr <- dplyr::filter(addr, !(level == lvl & tag %in% tags))
+
+  # Only use tags which are needed
+  if(length(tags) > length(new_value)) length(tags) <- length(new_value)
+  
+    # Add again
+  new_addr <- new_addr %>% 
+    tibble::add_row(tibble::tibble(record = addr$record[1], level = lvl, tag = tags, value = new_value)) %>% 
+    # Order by tags
+    dplyr::mutate(tag = factor(tag, levels = tag_order, ordered = TRUE)) %>% 
+    dplyr::arrange(tag) %>% 
+    dplyr::mutate(tag = as.character(tag))
+  
+  shiny::isolate(
+    r$ged <- ged %>% 
+      tibble::add_row(new_addr, .before = section$rows[1])
+  )
+  
+  section$rows <- seq.int(section$rows[1], length.out = nrow(new_addr))
+  TRUE
+}
+
+split_multiline <- function(multiline, max_lines) {
+
+  multiline <- unlist(strsplit(multiline, "\n"))
+  multiline <- unique(multiline[multiline != ""])
+  if(length(multiline) > max_lines) multiline <- multiline[1:max_lines]
+  multiline
+}
